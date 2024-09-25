cea2fe66ce94c31c6828b92895d6ca3b437afbc1
imkoolya <imkoolya@gmail.com>
2024-09-25 21:07:05 +0300

    version 2.0

--- WeatherApp/WeatherApp/AlarmPage.xaml.cs
+++ WeatherApp/WeatherApp/AlarmPage.xaml.cs
@@ -15,21 +15,25 @@ namespace WeatherApp
 
         public void GetContent()
         {
-            var datePickerText = new Label { Text = "Дата запуска", Margin = new Thickness(0, 20, 0, 10), HorizontalOptions = LayoutOptions.Center };
+            var datePickerText = new Label { Text = "Дата запуска", FontSize = 20, HorizontalOptions = LayoutOptions.Center };
             var datePicker = new DatePicker
             {
                 Format = "D",
                 MaximumDate = DateTime.Now.AddDays(7),
                 MinimumDate = DateTime.Now.AddDays(-7),
+                Style = (Style)App.Current.Resources["ValidInputStyle"]
             };
+            datePicker.PropertyChanged += (sender, e) => DateChanged(sender, e, datePicker);
             layout.Children.Add(datePickerText);
             layout.Children.Add(datePicker);
 
-            var timePickerText = new Label { Text = "Время запуска", Margin = new Thickness(0, 20, 0, 10), HorizontalOptions = LayoutOptions.Center };
+            var timePickerText = new Label { Text = "Время запуска", FontSize = 20, HorizontalOptions = LayoutOptions.Center };
             var timePicker = new TimePicker
             {
-                Time = new TimeSpan(13, 0, 0)
+                Time = new TimeSpan(13, 0, 0),
+                Style = (Style)App.Current.Resources["ValidInputStyle"]
             };
+            timePicker.PropertyChanged += (sender, e) => TimeChanged(sender, e, timePicker);
             layout.Children.Add(timePickerText);
             layout.Children.Add(timePicker);
 
@@ -37,17 +41,20 @@ namespace WeatherApp
             {
                 Minimum = 0,
                 Maximum = 100,
-                Value = 30.0,
+                Value = 30,
                 ThumbColor = Color.DodgerBlue,
                 MinimumTrackColor = Color.DodgerBlue,
                 MaximumTrackColor = Color.Gray,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                WidthRequest = 300
             };
-            var volumeText = new Label { Text = $"Громкость: {volume.Value}", Margin = new Thickness(0, 20, 0, 10), HorizontalOptions = LayoutOptions.Center };
+            var volumeText = new Label { Text = $"Громкость: {volume.Value}", FontSize = 20, HorizontalOptions = LayoutOptions.Center };
             volume.ValueChanged += (sender, e) => TempChangedHandler(sender, e, volumeText);
             layout.Children.Add(volumeText);
             layout.Children.Add(volume);
-
-            var repeatText = new Label { Text = "Повторять ежедневно", HorizontalOptions = LayoutOptions.Center, Margin = new Thickness(0, 5, 0, 0) };
+            
+            var repeatText = new Label { Text = "Повторять ежедневно", FontSize = 20, HorizontalOptions = LayoutOptions.Center };
             Switch repeat = new Switch
             {
                 IsToggled = false,
@@ -58,12 +65,64 @@ namespace WeatherApp
             layout.Children.Add(repeatText);
             layout.Children.Add(repeat);
 
-            layout.Children.Add(new Button { Text = "Сохранить", BackgroundColor = Color.Silver, CornerRadius = 15 });
+            var saveButton = new Button 
+            { 
+                Text = "Сохранить",
+                FontSize = 20, 
+                BackgroundColor = Color.Silver, 
+                CornerRadius = 20, 
+                VerticalOptions = LayoutOptions.Center, 
+                HorizontalOptions = LayoutOptions.Center, 
+                WidthRequest = 250,
+
+            };
+            saveButton.Clicked += (sender, eventArgs) => SaveButtonClicked(sender, eventArgs, new View[]
+            {
+                datePicker,
+                timePicker,
+                volume,
+                repeat
+            });
+            saveButton.Clicked += (s, e) => SaveAlarmHandler(s, e, datePicker.Date + timePicker.Time);
+            layout.Children.Add(saveButton);
         }
 
         private void TempChangedHandler(object sender, ValueChangedEventArgs e, Label volumeText)
         {
-            volumeText.Text = String.Format("Громкость: {0:F1}", e.NewValue);
+            volumeText.Text = String.Format("Громкость: {0}", e.NewValue);
+        }
+
+        void SaveAlarmHandler(object sender, EventArgs e, DateTime alarmDate)
+        {
+            var dateHeader = new Label { Text = $"Будильник сработает:", FontSize = 20, HorizontalTextAlignment = TextAlignment.Center };
+            var dateText = new Label { Text = $"{alarmDate.Day}.{alarmDate.Month}.{alarmDate.Year} в {alarmDate.Hour}:{alarmDate.Minute}", FontSize = 20, HorizontalTextAlignment = TextAlignment.Center };
+
+            layout.Children.Add(dateHeader);
+            layout.Children.Add(dateText);
+        }
+
+        private void DateChanged(object sender, EventArgs e, DatePicker datePicker)
+        {
+            if (datePicker.Date <= DateTime.Now)
+                VisualStateManager.GoToState(datePicker, "Invalid");
+
+            else
+                VisualStateManager.GoToState(datePicker, "Valid");
+        }
+
+        private void TimeChanged(object sender, EventArgs e, TimePicker timePicker)
+        {
+            if (timePicker.Time <= DateTime.Now.TimeOfDay)
+                VisualStateManager.GoToState(timePicker, "Invalid");
+
+            else
+                VisualStateManager.GoToState(timePicker, "Valid");
+        }
+
+        private void SaveButtonClicked(object sender, EventArgs e, View[] views)
+        {
+            foreach (var view in views)
+                view.IsEnabled = false;
         }
     }
 }
